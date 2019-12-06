92255015cd0c08805e80cc1cbb8e5ce82c7129c2
Miguel Ben <itsmigben@gmail.com>
2019-12-06 21:31:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(str)
-  arr = str.split(" ") # turning into an arr
+  arr = str.split(" ") # turning it into an arr
   new_arr = []
   
   arr.collect {|string| new_arr << string.reverse }
